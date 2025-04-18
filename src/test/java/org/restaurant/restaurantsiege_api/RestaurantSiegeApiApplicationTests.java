64d93bbfa0137cbaba974361095245db0fb87490
Liantsoa-Najaina <najaina.andriamampianina@gmail.com>
2025-04-18 11:15:26 +0300

    Initial commit

---
+++ src/test/java/org/restaurant/restaurantsiege_api/RestaurantSiegeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.restaurant.restaurantsiege_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantSiegeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
