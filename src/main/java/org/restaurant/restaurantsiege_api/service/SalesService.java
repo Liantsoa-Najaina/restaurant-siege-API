f6d13f5f2ec046a02b907fc683fb90f44fd86c00
JenniferTantely <hei.jennifer.2@gmail.com>
2025-04-19 17:42:13 +0300

    feat(best sales): sort by toalamount if if quantity equals

--- src/main/java/org/restaurant/restaurantsiege_api/service/SalesService.java
+++ src/main/java/org/restaurant/restaurantsiege_api/service/SalesService.java
@@ -5,6 +5,7 @@ import org.restaurant.restaurantsiege_api.model.Sale;
 import org.restaurant.restaurantsiege_api.repository.operations.SalesRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -13,7 +14,12 @@ public class SalesService {
     private final SalesRepository salesRepository;
 
     public List<Sale> getAllSales(Integer top) {
+        Comparator<Sale> bestSalesComparator = Comparator
+                .comparingInt(Sale::getQuantitySold).reversed()
+                .thenComparing(Comparator.comparingDouble(Sale::getTotalAmount).reversed());
+
         return salesRepository.getAllSales().stream()
+                .sorted(bestSalesComparator)
                 .limit(top)
                 .toList();
     }
